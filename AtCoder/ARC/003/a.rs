c519f755d23944982b45103906931aef8f83f0a6
task4233 <tks.m1205@gmail.com>
2018-09-10 22:58:21 +0900

    [ADD] add ARC 003 as rust

--- AtCoder/ARC/003/a.rs
+++ AtCoder/ARC/003/a.rs
@@ -1,28 +1,81 @@
-use std::io;
-
+// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+#[allow(unused_macros)]
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+ 
+#[allow(unused_macros)]
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+ 
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+ 
+#[allow(unused_macros)]
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+ 
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+ 
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+ 
+    ($next:expr, bytes) => {
+        read_value!($next, String).into_bytes()
+    };
+ 
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+ 
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
 
 fn main() {
-    // read_lineしてparse
-    let mut tmp: String;
-    io::stdin().read_line(&mut tmp).expect("Failed to read line.");
-    let n: i32 = tmp.parse().unwrap();
+    input! {
+        n: u64,
+        r: chars,
+    }
 
-    // read_line
-    let s: String;
-    io::stdin().read_line(&mut s).expect("Failed to read line.");
-
-    let mut sm: f32 = 0.0;
-    // forループができない……
-    for si in s {
-        match si {
-            'A' => sm += 4.0,
-            'B' => sm += 3.0,
-            'C' => sm += 2.0,
-            'D' => sm += 1.0,
-             _  => sm += 0.0,
+    let mut score: f64 = 0.0;
+    for i in 0..n {
+        match r[i as usize] {
+            'A' => { score += 4.0; }
+            'B' => { score += 3.0; }
+            'C' => { score += 2.0; }
+            'D' => { score += 1.0; }
+             _  => { score += 0.0; }
         }
     }
-    
-    println!("{}", (sm / (n as f32)));
-    
+
+    let ans: f64 = score / n as f64;
+    println!("{}", ans);
 }
