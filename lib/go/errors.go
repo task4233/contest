6b6300614566e697a25bb6ac18fa6106b42c6129
Takashi MIMA <29667656+task4233@users.noreply.github.com>
2021-06-15 23:47:36 +0900

    Update errors.go

--- lib/go/errors.go
+++ lib/go/errors.go
@@ -3,7 +3,5 @@ package main
 import "errors"
 
 var (
-	zeroDivErr     = errors.New("zero division is not allowed")
-	nilErr         = errors.New("nil pointer is not allowed")
-	negativeValErr = errors.New("negative value is not allowed")
+	ScanErr = errors.New("failed Scan")
 )
