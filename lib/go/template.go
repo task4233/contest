f9a21916492557b7e45b68f06693b128eb785502
Takashi MIMA <29667656+task4233@users.noreply.github.com>
2021-06-15 23:27:21 +0900

    Update template.go

--- lib/go/template.go
+++ lib/go/template.go
@@ -2,33 +2,47 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"sort"
 	"strconv"
 )
 
+// errors
+var (
+	ScanErr = errors.New("failed Scan")
+)
+
+// CLI manages given data
 type CLI struct {
 	n int
 	a []int
 }
 
+// NewCLI generates a pointer to CLI struct
 func NewCLI() *CLI {
 	return &CLI{}
 }
 
+// Scan scans given data and assgin cli field
+// validation is also done in this function
 func (c *CLI) Scan() error {
 	sc := bufio.NewScanner(os.Stdin)
 	sc.Split(bufio.ScanWords)
 
-	sc.Scan()
+	if !sc.Scan() {
+		return ScanErr
+	}
 	var err error
 	if c.n, err = strconv.Atoi(sc.Text()); err != nil {
 		return fmt.Errorf("failed to Atoi: %w", err)
 	}
 
 	for idx := 0; idx < c.n; idx++ {
-		sc.Scan()
+		if !sc.Scan() {
+			return ScanErr
+		}
 		val, err := strconv.Atoi(sc.Text())
 		if err != nil {
 			return fmt.Errorf("failed to Atoi: %w", err)
@@ -40,6 +54,8 @@ func (c *CLI) Scan() error {
 	return nil
 }
 
+// Solve solves given problem
 func (c *CLI) Solve() error {
+	
 	return nil
 }
